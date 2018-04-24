4f0dd3f0ebe25fa5b4128fb3319865265a2b1201
Kelsey Hightower <kelsey.hightower@gmail.com>
2018-04-24 06:56:58 +0000

    clean up

--- cleanup.sh
+++ cleanup.sh
@@ -5,18 +5,20 @@ kubectl delete deploy vault-0 vault-1
 kubectl delete configmap vault vault-0 vault-1
 kubectl delete secrets vault
 
-
 COMPUTE_REGION=$(gcloud config get-value compute/region)
 PROJECT_ID=$(gcloud config get-value project)
 
-gcloud compute addresses delete vault vault-0 vault-1 \
+gcloud compute addresses delete vault \
+  --quiet \
   --region ${COMPUTE_REGION}
 
-gcloud iam service-accounts delete vault-server@${PROJECT_ID}.iam.gserviceaccount.com
+gcloud iam service-accounts delete \
+  vault-server@${PROJECT_ID}.iam.gserviceaccount.com \
+  --quiet
 
 rm *.pem *.csr
 
-gsutil rm -r gs://${PROJECT_ID}-vault-storage/*
+gsutil -m rm -r gs://${PROJECT_ID}-vault-storage/*
 gsutil rb gs://${PROJECT_ID}-vault-storage
 
-gcloud container clusters delete vault
+gcloud container clusters delete vault --quiet
